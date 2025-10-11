f6a245b2b57f319086171a9406646c91e5234743
zherdev <andrey.zherdev@db.com>
2025-10-11 14:25:04 +0300

    init

---
+++ ai-code-review/spring-ai-code-review/src/main/kotlin/com/example/springaicodereview/SpringAiCodeReviewApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springaicodereview
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAiCodeReviewApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringAiCodeReviewApplication>(*args)
+}
---
+++ ai-code-review/spring-ai-code-review/src/test/kotlin/com/example/springaicodereview/SpringAiCodeReviewApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springaicodereview
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringAiCodeReviewApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
